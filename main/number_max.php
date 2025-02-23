c9dc0f5d3cb5274b23571c409dd73fd6ed3cf62d
ILNAZ-linux <sabirov.main@gmail.com>
2025-02-23 03:13:04 +0300

    переделал 2 задание

--- main/number_max.php
+++ main/number_max.php
@@ -1,9 +1,21 @@
 <?php
 $numbers = [123, 32, 34, 123, 43, 432.2, 2432, 543, 12, 10];
 
-rsort($numbers);
+$count;
 
-echo "Отсортированный массив:\n";
-print_r($numbers);
+foreach ($numbers as $element) {
+    $count++; 
+}
 
+$n = $count;
+
+for ($i = 0; $i < $n - 1; $i++) {
+    for ($j = 0; $j < $n - $i - 1; $j++) {
+        if ($numbers[$j] < $numbers[$j + 1]) {
+            $temp = $numbers[$j];
+            $numbers[$j] = $numbers[$j + 1];
+            $numbers[$j + 1] = $temp;
+        }
+    }
+}
 echo $numbers[2]; 
